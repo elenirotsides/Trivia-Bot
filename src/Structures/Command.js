8dd9dcc2f9860f9fb33696e7ecbbc9ae74a18cca
David Carpenter <carpenterd777@gmail.com>
2021-10-10 23:57:43 -0400

    add deny commands method to command adt

--- src/Structures/Command.js
+++ src/Structures/Command.js
@@ -11,4 +11,19 @@ export default class Command {
     async run(message, args) {
         throw new Error(`Command ${this.name} doesn't provide a run method!`);
     }
+
+    /**
+     * Returns true if there are commands in the command param
+     * that need to be denied. Sends a message in the channel about
+     * the invalid command.
+    */
+    denyCommands(message, [command]) {
+        if (command) {
+            const cmd = this.client.commands.get(command) || this.client.commands.get(this.client.aliases.get(command));
+
+            if (!cmd) message.channel.send(`Invalid Command named: \`${command}\``);
+            return true;
+        }
+        return false;
+    }
 }
